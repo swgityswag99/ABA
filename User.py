5bc7050b2cf85c1cc130059fef7c66adc857eca4
Jarvis Lu <jarvis01.lu@gmail.com>
2021-04-18 20:25:31 -0700

    Create User.py

---
+++ User.py
@@ -0,0 +1,63 @@
+class record:
+    def __init__(self, fn, ln, address):
+        self.fn = fn 
+        self.ln = ln
+        self.address = address
+
+    def edit_fn(self, new_name):
+        self.fn = new_name
+    def edit_ln(self, new_name):
+        self.ln = new_name
+    def edit_address(self, new_address):
+        self.address = new_address 
+
+class User:
+#takes in address database and prompts for delete record, add record, edit record
+
+    def edit_database(address_database):
+    #    record = input("Enter record id to manipulate: ")
+        option = input("Would you like to: \n 1) add record \n 2) delete record \n 3) edit record \n")
+         #add record
+        
+        if option == "1":
+            key = input("Enter Unique ID: ")
+            first = input("Enter First Name: ")
+            last = input("Enter Last Name: ")
+            address = input("Enter Address: ")
+            obj = record(first, last, address)
+            address_database[key] = obj
+        #delete record
+        elif option == "2":
+            deleter = input("Enter id of item to delete: ")
+            address_database.pop(deleter)
+        #edit record
+        elif option == "3":
+            user = input("Enter User ID: ")
+            option2 = input("Would you like to: \n 1) edit first name \n 2) edit last name \n 3) edit address \n ")
+            if option2 == "1":
+                name = input("Enter new first name: ")
+                address_database[user].fn = name
+            elif option == "2":
+                namee = input("Enter new last name: ")
+                address_database[user].ln = namee
+            elif option == "3":
+                address = input("Enter new address: ")
+                address_database[user].address = address
+
+""" 
+class Test:   
+    def main():
+        guy = record("jake", "garcia", "11900 Coronado Ave") 
+        jake = record("james", "charles", "fox st") 
+        dict = {
+            "1" : guy,
+            "2" : jake
+        }
+
+        User.edit_database(dict)
+        print(dict["1"].address)
+        
+
+    if __name__ == "__main__":
+        main()
+    """
