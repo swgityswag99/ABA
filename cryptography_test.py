9886ea8deaadcedaef327870cc06c1576cdd47b7
chadmondwu <chadmondwu@gmail.com>
2021-04-19 22:19:50 -0700

    another crypto test

--- cryptography_test.py
+++ cryptography_test.py
@@ -1,82 +1,25 @@
 #pip install cryptography
 #Asymmetic Encryption
-#Source: nitratine.net
 
+import base64
+import os
 import cryptography
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-private_key = rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
-            backend=default_backend()
-        )
-public_key = private_key.public_key()
+password = b"password"
+salt = os.urandom(16)
 
-# Storing the keys
-
-pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-with open('private_key.pem', 'wb') as f:
-    f.write(pem)
-
-pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-with open('public_key.pem', 'wb') as f:
-    f.write(pem)
-
-# Reading the keys back in (in other modules)
-with open("private_key.pem", "rb") as key_file:
-        private_key = serialization.load_pem_private_key(
-            key_file.read(),
-            password=None,
-            backend=default_backend()
-        )
-with open("public_key.pem", "rb") as key_file:
-        public_key = serialization.load_pem_public_key(
-            key_file.read(),
-            backend=default_backend()
-        )
-
-# Encrypting: in main module
-f = open('test.txt', 'rb')
-message = f.read()
-f.close()
-
-encrypted = public_key.encrypt(
-        message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-f = open('test.encrypted', 'wb')
-f.write(encrypted)
-f.close()
-
-
-#Decrypting: in modules
-f = open('test.encrypted', 'rb')
-decrypted_message = f.read()
-f.close()
-
-original_message = private_key.decrypt(
-        decrypted_message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-
-f = open('decrypted_message.txt', 'wb')
-f.write(original_message)
-f.close()
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+)
+key = base64.urlsafe_b64encode(kdf.derive(password))
+f = Fernet(key)
+token = f.encrypt(b"Secret message!")
+print(token)
+d = f.decrypt(token)
+print(d.decode())
